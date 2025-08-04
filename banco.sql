1c8af4d3d07cbbe83982471a787f569604fc1cb2
RPMAX Solution <edusantos_sp@hotmail.com>
2025-08-04 12:20:52 -0300

    dd

---
+++ banco.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE estoque;
+
+USE estoque;
+
+CREATE TABLE produtos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    preco DECIMAL(10,2) NOT NULL,
+    quantidade INT NOT NULL,
+    categoria VARCHAR(50) NOT NULL,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
